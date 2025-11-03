54816913277a0bbb28259528a37978f6cf62b2bc
valantynrocher <valentinrocher@gmail.com>
2025-11-03 21:25:28 +0100

    styles: fixed tailwind config and theme customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,14 @@
+// ./tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ["var(--font-inter)", "sans-serif"],
-      displayTech: ["var(--font-space-grotesk)", "sans-serif"],
-      displayExplore: ["var(--font-playfair)", "serif"],
-    },
-    extend: {
-      colors: {
-        background: "var(--bg-default)",
-        paper: "var(--bg-paper)",
-        accent: "var(--accent)",
-        text: {
-          primary: "var(--text-primary)",
-          secondary: "var(--text-secondary)",
-          button: "var(--text-button)", // Couleur spécifique pour le texte des boutons
-        },
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    // OBJET VIDE !
+    // La V4 gère le thème dans globals.css
   },
   plugins: [],
 };
